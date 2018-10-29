a606a9b8562e96873a72e96af8afd1cd16a23d72
bob <admin@TA-828-09>
2018-10-29 15:52:26 +0100

    mine opgaver

---
+++ TaleOpgave/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaleOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            a++;
+            a--;
+            a += 20;
+            Console.WriteLine(a);
+
+            double b = 12.5;
+            b++;
+            b--;
+            b *= 2;
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ VariabelDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariabelDemo
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+
+            int a;
+            a = 10;
+            long b;
+            b = 10000;
+
+            string f;
+
+            bool c = true;
+
+            byte d = 50;
+
+            f = Convert.ToString(b);            
+
+            
+            Console.WriteLine(Convert.ToSingle(a));
+            Console.WriteLine(f);
+
+            Console.ReadKey();
+        }
+    }
+}
