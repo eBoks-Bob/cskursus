788686de84663dc4be5201b04f024fffd3c5a96b
bob <admin@TA-828-09>
2018-10-30 15:56:21 +0100

    mine opg

---
+++ SimpleOpgave/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = LægSammen(5, 2);
+            Console.WriteLine(res);
+
+            double res2 = BeregnAreal(5);
+            Console.WriteLine(res2);
+
+            Udskriv("Dette er en test");
+        }
+
+        static int LægSammen(int a, int b)
+        {
+            return a + b;
+        }
+
+        static double BeregnAreal(int radius)
+        {
+            return radius * radius * System.Math.PI;
+        }
+
+        static void Udskriv(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+    }
+}
